8e928510f00ed8bae8cfaec9a2470c53eddd54ec
Amoodaa <amoodaa@gmail.com>
2019-07-08 11:17:32 +0300

    solved

--- exercises/exercise1.js
+++ exercises/exercise1.js
@@ -1,4 +1,5 @@
 // leave me alone :) :) :) <3 <3 <3
+
 var constantNumber = 5;
 var constantArray = [5, 7, 23, 4];
 var constantObject = {
@@ -8,37 +9,46 @@ var constantObject = {
 };
 // leave me alone :) :) :) <3 <3 <3
 
+// const ourGlobalVar = {
+//   constantNumber : 5,
+//   constantArray : [5, 7, 23, 4],
+//   constantObject : {
+//     "a": 5,
+//     "b": 2,
+//     "c": 8
+//   },
+// };
 // Refactor the following functions into pure functions:
 
-var addOne = function () {
-  constantNumber += 1;
-  return constantNumber;
+var addOne = function (num) {
+  return num + 1;
 };
 
 
-var timesTwo = function () {
-  constantNumber = constantNumber * 2
-  return constantNumber
+var timesTwo = function (num) {
+  return num * 2;
 };
 
 
 var incrementArray = function (array) {
-  array.forEach(function(x, i) {
-    array[i] =  x + 1;
+  return array.map(e =>{
+    return e+1;
   })
-  return array
 }
 
 var addNumberArray = function (array, number) {
-  array.push(number);
-  return array;
+  let newArray = [...array];
+  newArray.push(number);
+  return newArray;
 };
 
 var incrementObject = function (object) {
-  Object.keys(object).forEach(function(x) {
-    object[x] = object[x] + 1
+  // let newObj = Object.assign({},object);
+  let newObj = {...object};
+  Object.keys(newObj).forEach(function (x) {
+    newObj[x] = newObj[x] + 1;
   });
-  return object;
+  return newObj;
 };
 
 module.exports = {
